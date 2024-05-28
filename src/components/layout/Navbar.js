4104de49640c626230c63311f4853339dfc22ff2
nghiamai <nghiamaiquangtri@gmail.com>
2024-05-28 20:36:35 +0700

    create folder components and Navbar.js

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Navbar = () => {
+return (
+<nav className="navbar bg-success">
+<h1>
+<i className="fab fa-github" /> GitHub Finder
+</h1>
+<ul>
+
+<li>
+<a href="#>">Home</a>
+<a href="#>">About</a>
+</li>
+</ul>
+</nav>
+);
+};
+export default Navbar;
