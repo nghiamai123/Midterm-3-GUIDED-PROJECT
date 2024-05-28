f937e693410ad132b348ff7f7121db40b975a881
nghiamai <nghiamaiquangtri@gmail.com>
2024-05-28 21:18:15 +0700

    add function ClearsUsers

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -2,39 +2,54 @@ import axios from "axios";
 import React, { useState } from "react";
 import Users from "./Users";
 const Search = () => {
-const [text, setText] = useState("");
-const [users, setUsers] = useState([]);
+    const [text, setText] = useState("");
+    const [users, setUsers] = useState([]);
 
-const searchUsers = async (text) => {
-    try {
-        const response = await axios.get(`https://api.github.com/search/users?q=${text}`);
-        setUsers(response.data.items);
-        } 
-    catch (error) {
-            console.error("Error fetching data:", error);
-        }
-    };
-    const onSubmit = (e) => {
-        e.preventDefault();
+    const searchUsers = async (text) => {
+        try {
+            const response = await axios.get(`https://api.github.com/search/users?q=${text}`);
+            setUsers(response.data.items);
+            } 
+        catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        const onSubmit = (e) => {
+            e.preventDefault();
         if (text === "") {
             alert("Please enter something");
         } 
         else {
             searchUsers(text);
             setText("");
-    }
-};
+        }
+    };
+    const clearUsers = () => {
+        setUsers([]);
+    };
 
-const onChange = (e) => setText(e.target.value);
+    const onChange = (e) => setText(e.target.value);
 
 return (
     <div>
-        <form onSubmit={onSubmit} className="form">
-            <input type="text" name="text" placeholder="Search User" value={text} onChange={onChange}/>
-
-            <input type="submit" value="Search" className="btn btn-success btn-block" />
-        </form>
-    <Users users={users} />
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search User"
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-success btn-block"
+        />
+      </form>
+      {users.length > 0 && (
+            <button className="btn btn-danger btn-block" onClick={clearUsers}>Clear</button>
+        )}
+      <Users users={users} />
     </div>
     );
 };
